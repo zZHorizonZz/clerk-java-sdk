147f4657f8fb907841dd18c2f2dc33ae2f9fc448
zZHorizonZz <danfiala23@gmail.com>
2024-01-25 09:38:07 +0100

    docs(jwk): added docs to JWKService

--- src/main/java/io/github/zzhorizonzz/sdk/jwk/JWKService.java
+++ src/main/java/io/github/zzhorizonzz/sdk/jwk/JWKService.java
@@ -7,15 +7,31 @@ import org.jose4j.jwk.JsonWebKeySet;
 
 import java.io.InputStream;
 
+/**
+ * Service class for managing JSON Web Keys (JWKs).
+ * This class provides functionalities to retrieve and manage JWKs,
+ * including caching for efficiency.
+ */
 public class JWKService extends BaseService {
 
     private final JWKCache jwksCache;
 
+    /**
+     * Constructs a JWKService with the given ClerkClient.
+     *
+     * @param clerkClient The ClerkClient instance used for communication with backend services.
+     */
     public JWKService(ClerkClient clerkClient) {
         super(clerkClient);
         this.jwksCache = new JWKCache();
     }
 
+    /**
+     * Retrieves all JSON Web Keys (JWKs) from the server.
+     *
+     * @return JsonWebKeySet containing all the JWKs.
+     * @throws Exception If there's an issue retrieving the JWKs.
+     */
     public JsonWebKeySet listAll() throws Exception {
         InputStream keys = getHttpClient().jwks().get();
         if (keys == null) {
@@ -26,6 +42,14 @@ public class JWKService extends BaseService {
         return new JsonWebKeySet(jwks);
     }
 
+    /**
+     * Retrieves a specific JSON Web Key (JWK) based on the Key ID (kid).
+     * If the key is not found in the cache, it refreshes the cache from the server.
+     *
+     * @param kid The Key ID of the JWK to retrieve.
+     * @return The JsonWebKey corresponding to the provided Key ID.
+     * @throws Exception If there's an issue retrieving the JWK.
+     */
     public JsonWebKey getJWK(String kid) throws Exception {
         if (jwksCache.isInvalid()) {
             JsonWebKeySet jwks = listAll();
